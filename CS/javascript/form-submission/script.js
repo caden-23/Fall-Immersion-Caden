a3ab0d338180310fa4e2b141598ab9e4c4a17950
caden-23 <caden.lee@hgs.hiddengeniusproject.org>
2024-10-12 13:26:34 -0700

    Update script.js

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,15 @@
-function formSubmission() {
-    // Your code here.
-}
+const form = document.getElementById('myForm');
+addEventListener("sumbit", function(event){
+    event.preventDefault()
+
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('last name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (name && lastname && email && message){
+        alert('form submit successfully!\nName: ${name}\nlastname: ${last name}\nEmail: ${email}\nMessage ${Message}');
+    } else {
+        alert('Please fill all the fields');
+    }
+})
